82bbb745ce165cc71c56a38ff3f6fdc23a5c2f17
prarthna2803 <prarthna24427@iiitd.ac.in>
2025-12-27 02:35:09 +0530

    Update r_type.py

--- src/assembler/r_type.py
+++ src/assembler/r_type.py
@@ -1,1 +1,13 @@
+from .tables import func3, opcode, registers
 
+
+def convert_r_type(op, parts):
+    rd = registers[parts[1]]
+    rs1 = registers[parts[2]]
+    rs2 = registers[parts[3]]
+
+    funct7 = "0100000" if op == "sub" else "0000000"
+    funct3_val = func3["R-Type"][op]
+    op_val = opcode["R-Type"][op]
+
+    return f"{funct7}{rs2}{rs1}{funct3_val}{rd}{op_val}"
